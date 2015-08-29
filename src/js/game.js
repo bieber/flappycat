897fa6ebeb3e14eea987023ee6a0a407aba67623
Robert Bieber <robby@bieberphoto.com>
2015-08-28 22:05:55 -0700

    Initial Commit
    Set up the project, added a basic game class with rendering and
    time-advance functions.

---
+++ src/js/game.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015, Robert Bieber
+ *
+ * This file is part of flappycat.
+ *
+ * flappycat is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * flappycat is distributed in the hope that it will be useful,
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with flappycat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @flow
+ */
+
+var g = 0.2;
+
+export default class Game {
+	player: {
+		y: number;
+		vy: number;
+	};
+
+	constructor() {
+		this.player = {y: .5, vy: 0};
+	}
+
+	tick(dt: number) {
+		var {y, vy} = this.player;
+		y += vy * dt;
+		vy += dt * g;
+		this.player = {y, vy};
+	}
+	
+	render(context: CanvasRenderingContext2D) {
+		var {width, height} = context.canvas;
+
+		context.clearRect(0, 0, width, height);
+		
+		context.fillStyle = 'black';
+		context.fillRect(0, 0, width, height);
+
+		//Drawing the player
+		context.fillStyle = 'white';
+		context.strokeStyle = 'white';
+		var playerCenterX = 0.1 * width;
+		var playerCenterY = this.player.y * height;
+		var playerRadius = Math.min(width, height) * 0.01;
+
+		context.beginPath();
+		context.arc(playerCenterX, playerCenterY, playerRadius, 0, Math.PI * 2);
+		context.stroke();
+	}
+}
---
+++ src/js/init.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015, Robert Bieber
+ *
+ * This file is part of flappycat.
+ *
+ * flappycat is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * flappycat is distributed in the hope that it will be useful,
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with flappycat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @flow
+ */
+
+import Game from './game.js';
+
+var lastTime: ?number = null;
+
+function resizeCanvas(canvas: HTMLCanvasElement) {
+	canvas.width = document.documentElement.clientWidth;
+	canvas.height = document.documentElement.clientHeight - 30;
+}
+
+function frame(game: Game, canvas: HTMLCanvasElement, timeStamp: number) {
+	if (lastTime) {
+		var dt = (timeStamp - lastTime) / 1000;
+		game.tick(dt);
+		lastTime = timeStamp;
+	} else {
+		lastTime = timeStamp;
+	}
+
+	var context = canvas.getContext('2d');
+	if (!(context instanceof CanvasRenderingContext2D)) {
+		throw new Error('Rendering context error');
+	}
+	game.render(context);
+	
+	window.requestAnimationFrame(frame.bind(null, game, canvas));
+}
+
+export default function init() {
+	var canvas = document.getElementById('canvas');
+	if (!(canvas instanceof HTMLCanvasElement)) {
+		throw new Error('Canvas element must be a canvas');
+	}
+
+	var input = document.getElementById('input');
+	if (!(input instanceof HTMLInputElement)) {
+		throw new Error('input must be an input element');
+	}
+	
+	resizeCanvas(canvas);
+	window.onresize = resizeCanvas.bind(null, canvas);
+
+	var game = new Game();
+	window.requestAnimationFrame(frame.bind(null, game, canvas));
+}
